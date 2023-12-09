588f63af464c6434ab3ed634973ae5757e9c396a
hikmethankolay <hikmethan.kolay@gmail.com>
2023-12-09 18:57:16 +0300

    .

--- src/car_maintenance_app/src/car_maintenance_app.cpp
+++ src/car_maintenance_app/src/car_maintenance_app.cpp
@@ -3,9 +3,9 @@
 #include <string>
 #include <sstream>
 #include <stdexcept>
+#include <codecvt>
 #include "../../car_maintenance_lib/include/car_maintenance_lib.h"  // Adjust this include path based on your project structure
 
 
 int main() {
-  file_write("deneme.txt", "DENEME");
 }
