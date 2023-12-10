0f54f79bc1678644963e10fed6b01b1721059e5c
hikmethankolay <hikmethan.kolay@gmail.com>
2023-12-10 17:27:28 +0300

    Deleted arrow key menu.

--- src/car_maintenance_app/src/car_maintenance_app.cpp
+++ src/car_maintenance_app/src/car_maintenance_app.cpp
@@ -8,71 +8,63 @@
 #include <conio.h>
 #include "../../car_maintenance_lib/include/car_maintenance_lib.h"  // Adjust this include path based on your project structure
 
-/**
- * @brief Diplays the menu for operations.
- *
- */
-void displayMenu(const char *options[], int size, int selected) {
-  system("cls"); // Clears the console screen
-  std::cout << "----------Main Menu----------\n";
-
-  for (int i = 0; i < size; ++i) {
-    if (i == selected) {
-      std::cout << options[i] << " <--" << std::endl;
-    } else {
-      std::cout << options[i] << std::endl;
-    }
-  }
-}
 
 /**
  * @brief Car maintenance Application main function.
  *
  */
 int main() {
-  const char *menuOptions[] = {
-    "1-)Login",
-    "2-)Register",
-    "3-)Change Password",
-    "4-)Exit"
-  };
-  const int menuSize = sizeof(menuOptions) / sizeof(menuOptions[0]);
-  int selectedOption = 0;
-  char key;
+login:
+  int selectedOption;
+  int selectedOption2;
+  cout << "----------Login----------\n";
+  cout << "1-)Login\n";
+  cout << "2-)Register\n";
+  cout << "3-)Change Password\n";
+  cout << "4-)Exit\n";
+  cout << "Make a choice(1-4)";
+  cin >> selectedOption;
 
-  do {
-    displayMenu(menuOptions, menuSize, selectedOption);
-    key = _getch(); // Get a character from the console without waiting for Enter
-
-    switch (key) {
-      case 72: // Up arrow key
-        selectedOption = (selectedOption - 1 + menuSize) % menuSize;
-        break;
-
-      case 80: // Down arrow key
-        selectedOption = (selectedOption + 1) % menuSize;
-        break;
-    }
-  } while (key != 13); // 13 is the ASCII code for Enter key
-
-  // Handle selected option
   switch (selectedOption) {
-    case 0:
-      std::cout << "Login selected.\n";
-      // Handle User Authentication
-      break;
-
     case 1:
-      std::cout << "Register selected.\n";
-      // Handle Service History Tracking
-      break;
+      user_login();
+
+      if (user_login() == 0) {
+main_menu:
+        cout << "----------Main Menu----------\n";
+        cout << "1-)Service History Tracking\n";
+        cout << "2-)Maintenance Reminders\n";
+        cout << "3-)Expense Logging:\n";
+        cout << "4-)Fuel Efficiency Reports\n";
+        cout << "5-)Exit\n";
+        cout << "Make a choice(1-6)";
+        cin >> selectedOption2;
+
+        switch (selectedOption2)
+        case 1:
+      case 2:
+      case 3:
+      case 4:
+      case 5: {
+        default:
+          goto main_menu;
+        }
+      } else {
+        goto login;
+      }
 
     case 2:
-      std::cout << "Change Password selected.\n";
-      // Handle Maintenance Reminders
-      break;
+      user_register();
+      goto login;
 
     case 3:
+      user_change_password();
+      goto login;
+
+    case 4:
       return 0;
+
+    default:
+      goto login;
   }
 }
